f8d5d739a8d88ee8d93cead8d8052f87dbbb986f
gsus <santiagoxgames@gmail.com>
2024-10-08 11:09:16 -0500

    calculator substract

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -6,11 +6,29 @@ import (
 )
 
 func TestAdd(
-	t *testing.T
+	t *testing.T,
 ) {
 	t.Parallel()
 	var want float64 = 4
 	got := calculator.Add(2, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+/*
+To a test to work-out
+you need
+- A file that ends in _test.go
+- functions starting with Test
+- A param that is of type *testing.T
+- returns nothing
+*/
+
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+	var want float64 = 2
+	got := calculator.Subtract(4, 2)
 	if want != got {
 		t.Errorf("want %f, got %f", want, got)
 	}
