a91bffd5bd0990d885304d209f8949937b5e7e21
gsus <satehortuamercado@gmail.com>
2024-10-10 19:14:46 -0500

    New function added

--- creditcard/creditcard.go
+++ creditcard/creditcard.go
@@ -1,9 +1,18 @@
 package creditcard
 
+import "fmt"
+
 type creditcard struct {
 	number string
 }
 
-func New(number string) (string, error) {
-	return "", nil
+func New(number string) (creditcard, error) {
+	if number == "" {
+		return creditcard{}, fmt.Errorf("Invalid card number: %q", number)
+	}
+	return creditcard{number}, nil
+}
+
+func (cc *creditcard) Number() string {
+	return cc.number
 }
--- creditcard/creditcard_test.go
+++ creditcard/creditcard_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 func TestNew(t *testing.T) {
+	t.Parallel()
 	want := "123456789"
 	cc, err := creditcard.New(want)
 	if err != nil {
@@ -14,5 +15,13 @@ func TestNew(t *testing.T) {
 	got := cc.Number()
 	if want != got {
 		t.Errorf("want %q, got %q", want, got)
+	}
+}
+
+func TestNewInvalid(t *testing.T) {
+	t.Parallel()
+	_, err := creditcard.New("")
+	if err == nil {
+		t.Fatal("want error for invalid creditcard number")
 	}
 }
